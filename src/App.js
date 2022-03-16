f1f08c6da0475b90e14c86a72b4dee3eb3ee792a
Shidul Islam <sishahin093@gmail.com>
2022-03-16 16:32:10 +0600

    45-3 Create Component, return HTML from a component

--- src/App.js
+++ src/App.js
@@ -15,31 +15,35 @@ const singerStyle = {
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>JSX</h1>
-        <div className="container">
-          <h3>Hello dude. How are you</h3>
-        </div>
-        <div className="music">
-          <p>Name: {number}</p>
-          <p style={singerStyle}>Name : {singer.name} , Job : {singer.job}</p>
-          <p style={{ color: 'blue', backgroundColor: 'yellow' }}>Name : {singer2.name} Job : {singer2.job}</p>
-        </div>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Person></Person>
+      <Person></Person>
+      <Person></Person>
+      <Person></Person>
+      <h3>THis is New Component .</h3>
+      <Friend></Friend>
+      <Friend></Friend>
+      <Friend></Friend>
     </div>
   );
 }
+
+function Person() {
+  return (
+    <div className='person'>
+      <h1>Sakib al hasan</h1>
+      <p>Profession : Cricketer</p>
+    </div>
+  );
+}
+
+function Friend() {
+  return (
+    <div className='container'>
+      <h1>Sharif </h1>
+      <p>Job : maramari.</p>
+    </div>
+  )
+}
+
 
 export default App;
