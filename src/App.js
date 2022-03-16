fa59e3c137ca0aad5f84f674c8aa010df680a68d
Shidul Islam <sishahin093@gmail.com>
2022-03-16 18:38:52 +0600

    45-5 (advanced) Create multiple components from an array of objects

--- src/App.js
+++ src/App.js
@@ -2,8 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 
 const number = 5555;
-const singer = { name: 'Br. Mahfux ', job: 'singer' };
-const singer2 = { name: 'Eva  tahman', job: 'singer2' };
+const singers = [
+  { name: 'Br. Mahfux ', job: 'singer' },
+  { name: 'Eva  tahman', job: 'singer2' },
+  { name: 'agun ', job: 'sopno' },
+  { name: 'Br. Mahfux ', job: 'singer' },
+
+
+];
+// const singer2 = { name: 'Eva  tahman', job: 'singer2' };
 
 // jsx style crarte 
 const singerStyle = {
@@ -13,11 +20,26 @@ const singerStyle = {
 
 
 function App() {
+  const nayok = ['Rubel', 'BappaRaz', 'shahin', 'Sharif', 'hridoy', 'Riyag', 'Rajjak'];
   return (
     <div className="App">
-      <Person name="Rubel " naika="Musomi"></Person>
+      {
+        // nayok.map(nayok => <li>{nayok}</li>)
+      }
+      {
+        // nayok.map(nayok => <Person name={nayok}></Person>)
+      }
+
+      {
+        singers.map(singer => <Person name={singer.name} ></Person>)
+      }
+
+      {/* <Person name={nayok[0]} naika="Musomi"></Person>
+      <Person name={nayok[1]} naika="sabnur"></Person>
+      <Person name={nayok[2]} naika="Mitu"></Person> */}
+      {/* <Person name="Rubel " naika="Musomi"></Person>
       <Person name="Bapparaj" naika="sabnur"></Person>
-      <Person name="shahin" naika="Mitu"></Person>
+      <Person name="shahin" naika="Mitu"></Person> */}
       <h3>THis is New Component .</h3>
       <Friend name="Mohammod Azad" job="Emam"></Friend>
       <Friend name="Sajahan" job="BD Army"></Friend>
@@ -33,11 +55,9 @@ function App() {
 }
 
 function Person(props) {
-  // console.log(props);
   return (
     <div className='person'>
       <h1>{props.name}</h1>
-      <p>naika : {props.naika}</p>
     </div>
   );
 }
